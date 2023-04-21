db7dd252070e439e9e6fc59dde8185b4e5008439
Cory Loken <cory@crunchybananas.com>
2023-04-21 10:35:01 -0400

    Update local-storage-adapter.js

--- addon/initializers/local-storage-adapter.js
+++ addon/initializers/local-storage-adapter.js
@@ -5,17 +5,17 @@ import {
 } from 'ember-local-storage/helpers/import-export';
 
 export function initialize() {
-  if (!Store.prototype._emberLocalStoragePatched) {
-    Store.reopen({
-      _emberLocalStoragePatched: true,
-      importData: function (json, options) {
-        return importData(this, json, options);
-      },
-      exportData: function (types, options) {
-        return exportData(this, types, options);
-      },
-    });
-  }
+//   if (!Store.prototype._emberLocalStoragePatched) {
+//     Store.reopen({
+//       _emberLocalStoragePatched: true,
+//       importData: function (json, options) {
+//         return importData(this, json, options);
+//       },
+//       exportData: function (types, options) {
+//         return exportData(this, types, options);
+//       },
+//     });
+//   }
 }
 
 export default {
